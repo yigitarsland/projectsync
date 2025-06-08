27f7b6bfe79e8f052330d898908c7120a3f67047
Yigit Arslan <yigitarsland@icloud.com>
2025-06-08 07:52:22 +0200

    Fix: Firebase token parsing

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+  const serviceAccount = require('../config/serviceAccountKey.json');
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
